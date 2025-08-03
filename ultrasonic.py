efdfde99e82840466e5a08586c3fa43fa730bcd2
watcharains <85811864+watcharains@users.noreply.github.com>
2025-08-03 15:21:15 +0700

    Create ultrasonic.py

---
+++ ultrasonic.py
@@ -0,0 +1,21 @@
+import machine
+from machine import Pin
+import utime
+
+# Setup UltraSonic
+trigger = Pin(16, Pin.OUT)
+echo = Pin(17, Pin.IN)
+
+def ultra():
+   global distance
+   trigger.low()
+   utime.sleep_us(2)
+   trigger.high()
+   utime.sleep_us(5)
+   trigger.low()
+   while echo.value() == 0:
+       signaloff = utime.ticks_us()
+   while echo.value() == 1:
+       signalon = utime.ticks_us()
+   timepassed = signalon - signaloff
+   distance = (timepassed * 0.0343) / 2
